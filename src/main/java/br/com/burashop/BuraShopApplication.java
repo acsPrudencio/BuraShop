f0682b69b746cd0869e5ad021123ce0703c95ce7
Antonio Carlos Soares Prudencio <antonio.prudencio@maida.health>
2023-01-31 13:21:57 -0300

    first commit

---
+++ src/main/java/br/com/burashop/BuraShopApplication.java
@@ -0,0 +1,13 @@
+package br.com.burashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuraShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuraShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/burashop/BuraShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.burashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuraShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
